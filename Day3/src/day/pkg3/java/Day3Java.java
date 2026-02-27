b8514baca843c289fa64b8b0253377a08ee6639f
sharanya-anand <sharanya8123@gmail.com>
2026-02-27 15:28:24 +0530

    Delete Day3 directory

--- Day3/src/day/pkg3/java/Day3Java.java
+++
@@ -1,8 +0,0 @@
-package day.pkg3.java;
-public class Day3Java {
-
-    public static void main(String[] args) {
-
-    }
-    
-}
--- Day3/src/day/pkg3/java/cat2.java
+++
@@ -1,7 +0,0 @@
-package day.pkg3.java;
-public class cat2 extends animal2 {
-     void meow(){
-     System.out.println("meowwww");
-
-     }
-}
